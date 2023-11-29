b3ddb75873e7c47ce97404fb9b9d128a10a12303
freshmea <freshmea@naver.com>
2023-11-29 09:06:44 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Thread_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(Program2.print_hello));
+            t.Start("A");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("A_Hello World!" + i);
+            }
+            Program2.print_hello("B");
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_test
+{
+    internal class Program2
+    {
+        public static void print_hello(object a)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine((string)a+"_Hello World!" + i);
+            }
+        }
+    }
+}
