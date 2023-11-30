9c10324784c75e901a8f8f8f983af944d1d6eae6
freshmea <freshmea@naver.com>
2023-11-30 13:38:14 +0900

    src pdf

---
+++ source/19/AsyncFileIO/MainApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncFileIO
+{
+    class MainApp
+    {
+        // 파일 복사 후 복사한 파일 용량 반환.
+        static async Task<long> CopyAsync(string FromPath, string ToPath)
+        {
+            using (
+                var fromStream = new FileStream(FromPath, FileMode.Open))
+            {
+                long totalCopied = 0;
+                
+                using (
+                    var toStream = new FileStream(ToPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024];
+                    int nRead = 0;
+                    while ((nRead  = 
+                        await fromStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                    {
+                        await toStream.WriteAsync(buffer, 0, nRead);
+                        totalCopied += nRead;
+                    }
+                }
+
+                return totalCopied;
+            }
+        }
+
+        static async void DoCopy(string FromPath, string ToPath)
+        {
+            long totalCopied = await CopyAsync(FromPath, ToPath);
+            Console.WriteLine($"Copied Total {totalCopied} Bytes.");
+        }
+
+        static void Main(string[] args)
+        {
+            int args_len = args.Length;
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage : AsyncFileIO <Source> <Destination>");
+                return;
+            }
+            for (int i = 0; i < args_len/2; i++)
+            {
+                Console.WriteLine(i);
+                DoCopy(args[i*2], args[i*2+1]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/19/UsingTask/MainApp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace UsingTask
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string srcFile;
+            try
+            {
+                srcFile = args[0];
+            } catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("사용법 : UsingTask.exe <소스파일>");
+                return;
+            } 
+            Action<object> FileCopyAction = (object state) =>
+            {
+                String[] paths = (String[])state;
+                try
+                {
+                    File.Copy(paths[0], paths[1]);
+                }
+                catch (FileLoadException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                    Console.WriteLine();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+                Console.WriteLine("TaskID:{0}, ThreadID:{1}, {2} was copied to {3}",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId,
+                    paths[0], paths[1]);
+            };
+
+            Task t1 = new Task(
+                FileCopyAction, 
+                new string[]{srcFile, srcFile+".copy1"});
+
+            Task t2 = Task.Run(() => 
+            { 
+                FileCopyAction(new string[] { srcFile, srcFile + ".copy2" }); 
+            });
+            
+            t1.Start();
+
+            Task t3 = new Task(
+                FileCopyAction, 
+                new string[] { srcFile, srcFile + ".copy3" });
+
+            t3.RunSynchronously();
+
+            t1.Wait();
+            t2.Wait();
+            t3.Wait();
+
+
+            var myTask = Task<List<int>>.Run(
+                () =>
+                {
+                    Thread.Sleep(1000);
+
+                    List<int> list = new List<int>();
+                    list.Add(3);
+                    list.Add(4);
+                    list.Add(5);
+
+                    return list;
+                }
+            );
+
+            myTask.Wait();
+
+        }
+    }
+}
